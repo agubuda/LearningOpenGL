53cb77d88d87b78c7faed84578a395477cfd9fd1
agubuda <liu.wangyang@outlook.com>
2023-02-14 18:30:41 +0800

    error sys.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,6 +5,27 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << ")" << function << 
+            " "<< file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
@@ -33,7 +54,6 @@ static ShaderProgramSource ParseShader(const std::string& filepath)
 
             else if (line.find("fragment") != std::string::npos)
                 type = ShaderType::FRAGMENT;
-
         }
         else
         {
@@ -64,7 +84,6 @@ static unsigned CompileShader(const int type, const std::string& source)
         std::cout << message << std::endl;
         glDeleteShader(id);
         return 0;
-
     }
 
     return id;
@@ -137,8 +156,6 @@ int main(void)
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-    
-
     unsigned int shader = CreatShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
@@ -148,7 +165,9 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLClearError();
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+        //ASSERT(GLLogCall());
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
